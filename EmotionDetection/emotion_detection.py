a1e74348927eb54508d1b556dbfee5768574f83a
oswnavarre <oswaldo.navarrete@gmail.com>
2025-03-22 01:40:41 -0400

    Terminado

--- EmotionDetection/emotion_detection.py
+++ EmotionDetection/emotion_detection.py
@@ -6,21 +6,23 @@ def emotion_detector(text_to_analyze):
     myobj = { "raw_document": { "text": text_to_analyze } }
     header = {"grpc-metadata-mm-model-id": "emotion_aggregated-workflow_lang_en_stock"}
     response = requests.post(url, json = myobj, headers=header)
-
-    formatted_response = json.loads(response.text)
     
-    if response.status_code == 200:        
+    # Convert response text into a dictionary
+    formatted_response = json.loads(response.text)
+    # Not blank entries    
+    if response.status_code == 200:  
+        # Extract the set of emotions      
         anger_score = formatted_response["emotionPredictions"][0]["emotion"]["anger"]
         disgust_score = formatted_response["emotionPredictions"][0]["emotion"]["disgust"]
         fear_score = formatted_response["emotionPredictions"][0]["emotion"]["fear"]
         joy_score = formatted_response["emotionPredictions"][0]["emotion"]["joy"]
         sadness_score = formatted_response["emotionPredictions"][0]["emotion"]["sadness"]
-
+        # Code logic to find the dominant emotion
         emotion_list = [anger_score, disgust_score, fear_score, joy_score, sadness_score]
         dominant_emotion_index = emotion_list.index(max(emotion_list))
         emotion_keys = ["anger", "disgust", "fear", "joy", "sadness"]
         dominant_emotion_key = emotion_keys[dominant_emotion_index]
-
+    # Blank entries
     elif response.status_code == 400:
         anger_score = None
         disgust_score = None
@@ -28,7 +30,7 @@ def emotion_detector(text_to_analyze):
         joy_score = None
         sadness_score = None
         dominant_emotion_key = None
-
+    # Return the output format required
     final_result = {
         'anger': anger_score,
         'disgust': disgust_score,
